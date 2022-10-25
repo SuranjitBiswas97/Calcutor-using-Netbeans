aa9f3ff600115775041d07e700ee98f903005980
SuranjitBiswas97 <suranjit.neub26@gmail.com>
2022-10-26 01:25:38 +0600

    initial commit

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,16 @@
+package calculator;
+
+/**
+ *
+ * @author User
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       CalculatorFrame frame = new CalculatorFrame();
+    }
+    
+}
